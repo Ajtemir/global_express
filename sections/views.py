d40f458ba4df75f7fd779cb728ea52953eb4bf31
aitemir <ajtemirulanbekuulu@gmail.com>
2021-10-09 19:06:22 +0600

    news and sale, about us are ready 2

--- sections/views.py
+++ sections/views.py
@@ -16,8 +16,6 @@ class AboutUsView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['object'] = AboutUsInformation.objects.first()
-        # object = AboutUsInformation.objects.first()
-        # print(object.images.all())
         return context
 
 
